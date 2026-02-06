3e1a55ef36161b7c6825a035d193e5ed89ac2757
Katie Day <katie.day.13@proton.me>
2026-02-06 16:55:00 -0500

    fix recommends

--- bin/install-emacs.sh
+++ bin/install-emacs.sh
@@ -2,6 +2,6 @@
 set -e -u -o pipefail
 
 # We don't want mailutils so...
-sudo apt install --no-recommends -y emacs
+sudo apt install --no-install-recommends -y emacs
 # We do want to build vterm
 sudo apt intall -y cmake libtool-bin libvterm-dev
