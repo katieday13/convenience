c82130da5efd0fb7adc525f9418e960b1947718f
Katie Day <katie.day.13@proton.me>
2026-02-06 16:55:38 -0500

    Spell install right

--- bin/install-emacs.sh
+++ bin/install-emacs.sh
@@ -4,4 +4,4 @@ set -e -u -o pipefail
 # We don't want mailutils so...
 sudo apt install --no-install-recommends -y emacs
 # We do want to build vterm
-sudo apt intall -y cmake libtool-bin libvterm-dev
+sudo apt install -y cmake libtool-bin libvterm-dev
